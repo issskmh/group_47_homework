5ebc80333879a9959a1c442dcc65d58c2fef5fb2
issskmh <meshanlomislam@gmail.com>
2024-10-20 17:10:06 +0600

    modified 6hw

--- hw6.py
+++ hw6.py
@@ -12,27 +12,34 @@ print(count)
 
 
 def binary_search(lst, search_item):
-    low = 1
+    low = 0
     high = len(lst) - 1
     search_res = False
 
     while low <= high and not search_res:
         middle = (low + high) // 2
         guess = lst[middle]
+
+        # Печать текущих значений
+        print(f"Low: {low}, Hihg: {high}, Midle: {middle}, Guess: {guess}")
+
         if guess == search_item:
             search_res = True
+            print(f"Element {search_item} found in index {middle}")
             return search_res
         if guess > search_item:
             high = middle - 1
         else:
             low = middle + 1
+
+    print(f"Element {search_item} not found")
     return search_res
 
 
 lst = [3, 5, 11, 12, 15, 23, 25, 34, 67, 86]
-value = 11
+value = 3
 result = binary_search(lst, value)
 if result:
-    print('element find!')
+    print('Element found!')
 else:
-    print("element not find")
+    print("Element not found")
